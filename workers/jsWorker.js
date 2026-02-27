958878c9f57fe3342dcb0bb432a738b57aafe784
LOL-lab48 <236125732+LOL-lab48@users.noreply.github.com>
2026-02-27 17:54:54 +1100

    Update jsWorker.js

--- workers/jsWorker.js
+++ workers/jsWorker.js
@@ -1,1 +1,31 @@
+// workers/jsWorker.js
 
+self.onmessage = function(e) {
+    const userCode = e.data;
+
+    let output = "";
+    let error = null;
+
+    // Override console.log to capture output
+    const originalConsoleLog = console.log;
+    console.log = function(...args) {
+        output += args.join(" ") + "\n";
+    };
+
+    try {
+        // Execute user code safely
+        const result = new Function(userCode);
+        result();
+    } catch (err) {
+        error = err.toString();
+    }
+
+    // Restore console
+    console.log = originalConsoleLog;
+
+    // Send result back
+    self.postMessage({
+        output: output.trim(),
+        error: error
+    });
+};
