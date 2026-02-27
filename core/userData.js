e5ae6fa297ba50ebe4d086678f5ae60b4d74a5e7
LOL-lab48 <236125732+LOL-lab48@users.noreply.github.com>
2026-02-27 20:26:07 +1100

    Update userData.js

--- core/userData.js
+++ core/userData.js
@@ -1,4 +1,13 @@
-export const userData = {
+// core/userData.js
+
+const savedData = JSON.parse(localStorage.getItem("bytepath48_user"));
+
+export const userData = savedData || {
     currentLesson: 1,
-    totalLessons: 180
+    totalLessons: 180,
+    lastUnlockDate: null
 };
+
+export function saveUserData() {
+    localStorage.setItem("bytepath48_user", JSON.stringify(userData));
+}
